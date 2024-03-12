552ea2f1619c7cc4fb7d06c351e181b6c903d853
Abhishek Anand <abhinitjsr035@gmail.com>
2024-03-12 17:43:26 +0530

    changes added and try catch added

--- routes/auth.py
+++ routes/auth.py
@@ -1,20 +1,23 @@
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
+from config.db import authentication_DB
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
-from config.db import authentication_DB
 from models.todos import User
 from typing import Optional
+
 
 router = APIRouter(
     prefix='/auth',
     tags=['auth']
 )
 
+
 SECRET_KEY = "9759423hihfgwiyt3yt347yy9ifn3y83y489yht8"  # Replace with a secret key
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
@@ -23,39 +26,56 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
+# function to get the user data from the authentication_DB.
 def get_user(username: str):
-    user_data = authentication_DB.find_one({"username": username})
-    if user_data:
-        return User(**user_data)
+    try:
+        user_data = authentication_DB.find_one({"username": username})
+        if user_data:
+            return User(**user_data)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     return None
 
+
+# function to authenticate the user coming with their username and the password.
 def authenticate_user(username: str, password: str):
-    user = get_user(username)
-    if not user or not verify_password(password, user.password):
-        return False
-    return user
+    try:
+        user = get_user(username)
+        if user and verify_password(password, user.password):
+            return user
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return False
 
+# for creating the access token
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+    try:
+        to_encode = data.copy()
+        if expires_delta:
+            expire = datetime.utcnow() + expires_delta
+        else:
+            expire = datetime.utcnow() + timedelta(minutes=15)
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+        return encoded_jwt
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
+# post method for logging for access token
 @router.post("/token")
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=401,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
+    try:
+        user = authenticate_user(form_data.username, form_data.password)
+        if not user:
+            raise HTTPException(
+                status_code=401,
+                detail="Incorrect username or password, Please enter correct username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": user.username}, expires_delta=access_token_expires
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
+        return {"access_token": access_token, "token_type": "bearer"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- routes/route.py
+++ routes/route.py
@@ -1,71 +1,90 @@
-from fastapi import APIRouter, HTTPException, status, Depends
-
+from fastapi import APIRouter, HTTPException, Depends
 from models.todos import Contact, User
 from config.db import collection_name, authentication_DB
 from schema.schemas import list_serial, user_serial
 from bson import ObjectId
-# from .auth import get_current_user
 from .auth import authenticate_user, oauth2_scheme
 from passlib.context import CryptContext
 
 router = APIRouter()
 
-# Store user name and password
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
+# Store user name and password
 @router.post("/add/user")
 async def Add_User(usr: User):
-    hashed_password = pwd_context.hash(usr.password)
-    new_user = User(username=usr.username, password=hashed_password)
-    authentication_DB.insert_one(dict(new_user))
-    return {"message": "User Added successfully"}
+    try:
+        hashed_password = pwd_context.hash(usr.password)
+        new_user = User(username=usr.username, password=hashed_password)
+        authentication_DB.insert_one(dict(new_user))
+        return {"message": "User Added successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 # Get User request
 @router.get('/')
 async def find_all_contact_of_current_user(current_user: User = Depends(authenticate_user)):
-    if not current_user:
-        raise HTTPException(status_code=401, detail="Invalid token")
-    todos = list_serial(collection_name.find({"username":current_user.username})) # find everything in the collection and return it
-    return todos
+    try:
+        if not current_user:
+            raise HTTPException(status_code=401, detail="Invalid token, Please come with correct credentials.")
+        todos = list_serial(collection_name.find({"username":current_user.username}))
+        return todos
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
-# POST request method Write in DB
-@router.post("/")
+
+# post request method for writing contacts of current user in the db.
 @router.post("/")
 async def Post_contact_of_current_user(todo: Contact, current_user: User = Depends(authenticate_user)):
-    if not current_user:
-        raise HTTPException(status_code=401, detail="Invalid token")
-    collection_name.insert_one(dict(todo))
-    return {"message": "User created successfully"}
+    try:
+        if not current_user:
+            raise HTTPException(status_code=401, detail="Invalid token, Please come with correct credentials.")
+        collection_name.insert_one(dict(todo))
+        return {"message": "User created successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
-# Put request  method Update in DB
+
+# Put request method Update in DB
 @router.put("/{id}")
 async def update_user(id: str, todo: Contact, current_user: User = Depends(authenticate_user)):
-    if not current_user:
-        raise HTTPException(status_code=401, detail="Invalid token")
-    user_data = collection_name.find_one(ObjectId(id))
-    if (user_data["username"] != current_user.username):
-        raise HTTPException(status_code=401, detail="Contact not present for the current user")
-    collection_name.find_one_and_update({"_id": ObjectId(id)}, {"$set": dict(todo)})
-    return {"message": "User updated successfully"}
+    try:
+        if not current_user:
+            raise HTTPException(status_code=401, detail="Invalid token, Please come with correct credentials.")
+        user_data = collection_name.find_one(ObjectId(id))
+        if (user_data["username"] != current_user.username):
+            raise HTTPException(status_code=401, detail="Contact not present for the current user")
+        collection_name.find_one_and_update({"_id": ObjectId(id)}, {"$set": dict(todo)})
+        return {"message": "User updated successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 # Delete request method
 @router.delete("/{id}")
 async def delete_User(id: str, current_user: User = Depends(authenticate_user)):
-    if not current_user:
-        raise HTTPException(status_code=401, detail="Invalid token")
-    user_data = collection_name.find_one(ObjectId(id))
-    if (user_data["username"] != current_user.username):
-        raise HTTPException(status_code=401, detail="Contact not present for the current user")
-    collection_name.find_one_and_delete({"_id": ObjectId(id)})
-    return {"message": "User deleted successfully"}
+    try:
+        if not current_user:
+            raise HTTPException(status_code=401, detail="Invalid token, Please come with correct credentials.")
+        user_data = collection_name.find_one(ObjectId(id))
+        if (user_data["username"] != current_user.username):
+            raise HTTPException(status_code=401, detail="Contact not present for the current user")
+        collection_name.find_one_and_delete({"_id": ObjectId(id)})
+        return {"message": "Contact deleted successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 @router.get("/search/relation/{relation}")
 async def search_user_by_relationship(relation: str, current_user: User = Depends(authenticate_user)):
-    if not current_user:
-        raise HTTPException(status_code=401, detail="Invalid token")
-    users = list_serial(collection_name.find({"Group":relation, "username":current_user.username}))
-    if not users:
-        raise HTTPException(status_code=404, detail="User not found")
-    return users
+    try:
+        if not current_user:
+            raise HTTPException(status_code=401, detail="Invalid token, Please come with correct credentials.")
+        users = list_serial(collection_name.find({"Group":relation, "username":current_user.username}))
+        if not users:
+            raise HTTPException(status_code=404, detail="Contacts not found")
+        return users
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
