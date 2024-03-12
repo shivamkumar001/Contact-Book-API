407b0202db730ca52291b4b368727c7bffacdd89
Abhishek Anand <abhinitjsr035@gmail.com>
2024-03-12 17:48:03 +0530

    comments added

--- models/todos.py
+++ models/todos.py
@@ -1,12 +1,14 @@
 from pydantic import BaseModel # data validation and manipulation
-from typing import Optional
-# from enum import Enum
+from typing import Optional   # Importing Optional from typing for optional fields
 
+
+# Define the User class which inherits from BaseModel for data validation
 class User(BaseModel):
     username: str
     password: str
 
-class Contact(BaseModel):
+# Define the Contact class which inherits from BaseModel for data validation
+class Contact(BaseModel):   
     ContactId: str
     name: Optional[str]
     MOB: str
